5263be1e7c60a2ea6e6466e2d5bdca383db9af5c
Unknown <salikvilmer@gmail.com>
2020-06-01 14:00:50 +0200

    filter search done

--- rental/views.py
+++ rental/views.py
@@ -2,20 +2,21 @@ from django.db.models import Q
 from django.shortcuts import render
 from .models import Property
 
+
 def index(request):
     if request.method == 'POST':
         if 'search_field' in request.POST:
-            search_inputs = request.POST['search_field'].replace(',', '').split(' ')
-            print(search_inputs)
-            for search_input in search_inputs:
-                if type(search_input) is 'int':
-                    search_part_properties = Property.objects.filter(Q(zip__zip=search_input))
-                else:
-                    search_part_properties = Property.objects.filter(Q(address__contains=search_input) |
-                                                                     Q(zip__city__name=search_input) |
-                                                                     Q(zip__city__country__name=search_input))
-                print(search_part_properties)
-    properties = Property.objects.all()
+            search_input = request.POST['search_field']
+            print(search_input)
+            if type(search_input) is 'int':
+                properties = Property.objects.filter(Q(zip__zip=search_input))
+            else:
+                properties = Property.objects.filter(Q(address__iexact=search_input) |
+                                                     Q(zip__city__name__iexact=search_input) |
+                                                     Q(zip__city__country__name__iexact=search_input))
+            print(properties)
+    else:
+        properties = Property.objects.all()
     context = {
         'properties': properties
     }
