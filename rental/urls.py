5062eac885971d087720554cdbd321aacef7d426
Marijn Pool <15004290+ph00lt0@users.noreply.github.com>
2020-06-01 13:24:30 +0200

    add booking form

--- rental/urls.py
+++ rental/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+app_name = 'rental'
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('property/<int:pk>', views.single_property, name='property'),
+    path('booking/create', views.create_booking, name='booking_create'),
 ]
--- rental/views.py
+++ rental/views.py
@@ -1,8 +1,12 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.decorators.http import require_http_methods
+from django.urls import reverse
 from django.shortcuts import render
-from .models import Property
+from .models import Property, Booking
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'rental/index.html')
 
 
 def single_property(request, pk):
@@ -13,3 +17,15 @@ def single_property(request, pk):
     }
 
     return render(request, 'rental/property.html', context)
+
+
+@require_http_methods(["POST"])
+def create_booking(request, *args, **kwargs):
+    Booking.objects.create(
+        customer=request.user,
+        property=Property.objects.get(pk=request.POST['property']),
+        start_date=request.POST['start_date'],
+        end_date=request.POST['end_date']
+    )
+
+    return HttpResponseRedirect(reverse('rental:index'))
